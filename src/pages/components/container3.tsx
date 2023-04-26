41f9c807f76b1d38cc68ea02a667f1d3df6d761e
ghazi-mkhinini <ghazimkini@gmail.com>
2023-04-26 16:02:02 +0100

    Fixed: minor inline style tag fixinig

--- src/pages/components/container3.tsx
+++ src/pages/components/container3.tsx
@@ -1,7 +1,54 @@
 export default function Container3({ text }): JSX.Element {
   return (
     <div className="inline-block m-2">
-      <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 922 536" width="922" height="536"><style>.a{fill:#3de0e0}.b{opacity:.4;fill:#3de0e0}</style><path class="a" d="m832.9 522h-338.2l-20-30.8h-202.7l-25 30.8h-223.6v-492.5h199.4v6.9h-192.6v478.8h213.6l24.9-30.8h209.7l20 30.8h331.5l70.7-77.6v-401.2h-121.4v-6.9h128.2v410.8z"/><path class="a" d="m457 40.5h-260.7l11.3-17.4h238.2z"/><path class="a" d="m215.5 33.9h-54.9v-8h54.9z"/><path class="a" d="m442.9 29.9h-222l8.3-15.9h205.4z"/><path class="a" d="m807.4 43.4h-114.7v-22.9h114.7z"/><path class="a" d="m73.3 533.7h-64.1v-85.1l15.3-8.8v78.6h61.2z"/><path class="a" d="m17.1 451.1h-4.6v-110.2h4.6z"/><path class="a" d="m17.1 215.1v136l-16.6-10v-116z"/><path class="a" d="m920.4 151.5l-16.4 10.6v-129.1h16.4z"/><path class="a" d="m916.2 211.3h-5.8v-62.9h5.8z"/><path class="a" d="m909.1 220.6v-6.2l12.9 4.3v6.1z"/><path class="a" d="m909.1 229.4v-6.1l12.9 4.2v6.1z"/><path class="a" d="m909.1 238.2v-6.1l12.9 4.2v6.2z"/><path class="a" d="m909.1 247.1v-6.2l12.9 4.3v6.1z"/><path class="a" d="m909.1 255.9v-6.1l12.9 4.2v6.1z"/><path class="a" d="m909.1 264.7v-6.1l12.9 4.2v6.2z"/><path class="a" d="m909.1 273.6v-6.2l12.9 4.3v6.1z"/><path class="a" d="m909.1 282.4v-6.1l12.9 4.2v6.1z"/><path class="a" d="m909.1 291.2v-6.1l12.9 4.2v6.2z"/><path class="a" d="m909.1 300.1v-6.1l12.9 4.2v6.1z"/><path class="a" d="m909.1 308.9v-6.1l12.9 4.2v6.1z"/><path class="a" d="m909.1 317.8v-6.2l12.9 4.2v6.2z"/><path class="a" d="m909.1 326.6v-6.1l12.9 4.2v6.1z"/><path class="a" d="m497.8 46h-29.2l20.1-46.1h29.2z"/><path class="a" d="m539.9 46h-29.2l20.2-46.1h29.2z"/><path class="a" d="m582.1 46h-29.3l20.2-46.1h29.2z"/><path class="a" d="m624.2 46h-29.2l20.1-46.1h29.3z"/><path class="a" d="m666.3 46h-29.2l20.2-46.1h29.2z"/><path class="a" d="m665.3 518.6h-148.8l7.5-17.5h133.9z"/><path class="a" d="m575.7 517.9h148.9l-7.5 17.5h-133.9z"/><path class="a" d="m43.3 156.7l-16.5 15v-138.7h16.5z"/><path class="a" d="m28.3 165.8l-20.2-14.8v-79.7l20.2-14.8z"/><path class="b" d="m60.4 60.2v108.7l-23.9 10.1v323.1h201.1l23.6-33.2h232.1l9.9 19.5h162.1l7.6 13.7h148.4l63.6-72.3v-369.6z"/></svg>
+      <svg
+        version="1.2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 922 536"
+        width="922"
+        height="536"
+      >
+        <style>{`.a{fill:#3de0e0}.b{opacity:.4;fill:#3de0e0}`}</style>
+        <path
+          className="a"
+          d="m832.9 522h-338.2l-20-30.8h-202.7l-25 30.8h-223.6v-492.5h199.4v6.9h-192.6v478.8h213.6l24.9-30.8h209.7l20 30.8h331.5l70.7-77.6v-401.2h-121.4v-6.9h128.2v410.8z"
+        />
+        <path className="a" d="m457 40.5h-260.7l11.3-17.4h238.2z" />
+        <path className="a" d="m215.5 33.9h-54.9v-8h54.9z" />
+        <path className="a" d="m442.9 29.9h-222l8.3-15.9h205.4z" />
+        <path className="a" d="m807.4 43.4h-114.7v-22.9h114.7z" />
+        <path className="a" d="m73.3 533.7h-64.1v-85.1l15.3-8.8v78.6h61.2z" />
+        <path className="a" d="m17.1 451.1h-4.6v-110.2h4.6z" />
+        <path className="a" d="m17.1 215.1v136l-16.6-10v-116z" />
+        <path className="a" d="m920.4 151.5l-16.4 10.6v-129.1h16.4z" />
+        <path className="a" d="m916.2 211.3h-5.8v-62.9h5.8z" />
+        <path className="a" d="m909.1 220.6v-6.2l12.9 4.3v6.1z" />
+        <path className="a" d="m909.1 229.4v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m909.1 238.2v-6.1l12.9 4.2v6.2z" />
+        <path className="a" d="m909.1 247.1v-6.2l12.9 4.3v6.1z" />
+        <path className="a" d="m909.1 255.9v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m909.1 264.7v-6.1l12.9 4.2v6.2z" />
+        <path className="a" d="m909.1 273.6v-6.2l12.9 4.3v6.1z" />
+        <path className="a" d="m909.1 282.4v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m909.1 291.2v-6.1l12.9 4.2v6.2z" />
+        <path className="a" d="m909.1 300.1v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m909.1 308.9v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m909.1 317.8v-6.2l12.9 4.2v6.2z" />
+        <path className="a" d="m909.1 326.6v-6.1l12.9 4.2v6.1z" />
+        <path className="a" d="m497.8 46h-29.2l20.1-46.1h29.2z" />
+        <path className="a" d="m539.9 46h-29.2l20.2-46.1h29.2z" />
+        <path className="a" d="m582.1 46h-29.3l20.2-46.1h29.2z" />
+        <path className="a" d="m624.2 46h-29.2l20.1-46.1h29.3z" />
+        <path className="a" d="m666.3 46h-29.2l20.2-46.1h29.2z" />
+        <path className="a" d="m665.3 518.6h-148.8l7.5-17.5h133.9z" />
+        <path className="a" d="m575.7 517.9h148.9l-7.5 17.5h-133.9z" />
+        <path className="a" d="m43.3 156.7l-16.5 15v-138.7h16.5z" />
+        <path className="a" d="m28.3 165.8l-20.2-14.8v-79.7l20.2-14.8z" />
+        <path
+          className="b"
+          d="m60.4 60.2v108.7l-23.9 10.1v323.1h201.1l23.6-33.2h232.1l9.9 19.5h162.1l7.6 13.7h148.4l63.6-72.3v-369.6z"
+        />
+      </svg>
     </div>
   );
 }
