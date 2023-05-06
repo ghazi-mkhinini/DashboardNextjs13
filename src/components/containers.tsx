6372c9988ace6965e102c72cec1d4825d8fecdda
ghazi-mkhinini <ghazimkini@gmail.com>
2023-05-06 20:38:36 +0100

    Added:containers pos displacement in style prop

--- src/components/containers.tsx
+++ src/components/containers.tsx
@@ -16,22 +16,24 @@ interface propsType {
 
 const Containers = (props: propsType): JSX.Element => {
   const { children, ...rest } = props;
-  const [isActive, setIsActive] = useState(true);
+  const [isActive, setIsActive] = useState(false);
   const scope = useContainerAnimation(isActive);
   //const table=
   const t = React.Children.toArray(children);
   const arrr = t.map((Item: any, index: number) => {
+    let customPositionDisplacement = {top:`${20*(index + 1)}px`, left:`${20*(index + 1)}px`};
     //----Returning new Element with JSX
     return (
       <Container2
-        id={"container2_"+(index + 1)}
+        id={"container2_" + (index + 1)}
         customClassName={"absolute"}
+        style={customPositionDisplacement}
       ></Container2>
     );
     //----Returning new Element with React cloneElement  imperative code
     //return cloneElement(Item, { special : "absolute" }, null);
   });
-  return <>{arrr}</>;
+  return <div className="relative">{arrr}</div>;
 };
 
 export default Containers;
