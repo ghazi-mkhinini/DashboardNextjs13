fdf16bfd92c5dfa88a31040ca5143d9d46fd19e0
ghazi-mkhinini <ghazimkini@gmail.com>
2023-05-07 03:04:30 +0100

    Fixed:Cloning elements with cloneElement

--- src/components/containers.tsx
+++ src/components/containers.tsx
@@ -18,7 +18,8 @@ const Containers = (props: propsType): JSX.Element => {
   const { children, ...rest } = props;
   const [isActive, setIsActive] = useState(false);
   const scope = useContainerAnimation(isActive);
-  //const table=
+
+  //-----Traversing children of type ReactNode[], customizing them and finally cloning them
   const t = React.Children.toArray(children);
   const arrr = t.map((Item: any, index: number) => {
     let customPositionDisplacement = {
@@ -26,16 +27,25 @@ const Containers = (props: propsType): JSX.Element => {
       left: `${10 * index}px`,
     };
     //----Returning new Element with JSX
-    return (
+    /*return (
       <Container2
         id={"container2_" + (index + 1)}
         customClassName={"absolute"}
         style={customPositionDisplacement}
       ></Container2>
-    );
+    );*/
     //----Returning new Element with React cloneElement  imperative code
-    //return cloneElement(Item, { special : "absolute" }, null);
+    return cloneElement(
+      Item,
+      {
+        customClassName: "absolute",
+        id: "container2_" + (index + 1),
+        style: customPositionDisplacement,
+      },
+      null
+    );
   });
+
   return <div className="relative">{arrr}</div>;
 };
 
