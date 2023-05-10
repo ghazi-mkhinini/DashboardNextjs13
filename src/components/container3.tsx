a21fe25334a5f6b2f860a0ffcb3237c609b02cb4
ghazi-mkhinini <ghazimkini@gmail.com>
2023-05-11 00:38:09 +0100

    Fixed: Container3 ClassName => className

--- src/components/container3.tsx
+++ src/components/container3.tsx
@@ -3,14 +3,14 @@ import { ReactNode, useEffect, useState } from "react";
 import { useMenuAnimation } from "./container3_Controller";
 
 interface Props {
-  ClassName?:string;
+  className?:string;
   children?: ReactNode;
   width: number;
   text?: string;
 }
 
 export default function Container3({
-  ClassName,
+  className,
   width,
   text,
   children,
@@ -73,7 +73,7 @@ export default function Container3({
     <>
       <div
         id="container3"
-        className={"container3 inline-block m-2 relative"+" "+ClassName}
+        className={"container3 inline-block m-2 relative"+" "+className}
         ref={scope2}
         style={{
           transform: "translateX(0%)",
