ed4abb07214c716c06011230bd46907a494778ac
ghazi-mkhinini <ghazimkini@gmail.com>
2023-06-05 02:09:31 +0100

    Refactored: Component TabletMockup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ import QueryRoute1 from "@/API/QueryRoute1";
 import { Dial, DialOptions } from "flowbite";
 import SpeedDial from "@/components/SpeedDial";
 import Stepper from "@/components/Stepper";
+import TabletMockup from "@/components/TabletMockup";
 
 const queryClient = new QueryClient();
 
@@ -96,24 +97,7 @@ export default function Home() {
 
         {/*<Central></Central>*/}
 
-        <div className="dark relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[454px] max-w-[341px] md:h-[682px] md:max-w-[512px]">
-          <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[72px] rounded-l-lg"></div>
-          <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
-          <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
-          <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
-          <div className="rounded-[2rem] overflow-hidden h-[426px] md:h-[654px] bg-white dark:bg-gray-800">
-            <img
-              src="https://flowbite.s3.amazonaws.com/docs/device-mockups/tablet-mockup-image.png"
-              className="dark:hidden h-[426px] md:h-[654px]"
-              alt=""
-            />
-            <img
-              src="https://flowbite.s3.amazonaws.com/docs/device-mockups/tablet-mockup-image-dark.png"
-              className="hidden dark:block h-[426px] md:h-[654px]"
-              alt=""
-            />
-          </div>
-        </div>
+        <TabletMockup></TabletMockup>
 
         <SpeedDial></SpeedDial>
 
